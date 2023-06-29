d63c2d36038e3ae4717957fb86a5ffa5315a0488
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-06-29 20:46:45 +0530

    finish mergesort in python

--- python/merge-sort.py
+++ python/merge-sort.py
@@ -1,3 +1,5 @@
+import math
+
 def merge(leftArr, rightArr):
     result = []
 
@@ -22,5 +24,17 @@ def merge(leftArr, rightArr):
 
     return result
 
+def merge_sort(arr):
+    if len(arr) == 1: return arr
+        
+    mid = math.floor(len(arr) / 2)
+    left = arr[0:mid]
+    right = arr[mid:]
+
+    return merge(merge_sort(left), merge_sort(right))
+
+
 print(merge([1,3,7,8], [0,4,5,6]))
 print(merge([1,3,7,8], [0,4,6,9]))
+
+print(merge_sort([123,1234,98,1,89,23,8,12]))
